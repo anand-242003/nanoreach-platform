da8f6a17ead6fe7e6fd7e600daa0804fafb52f36
Anand Mishra <Anandmishra3001@gmail.com>
2026-01-11 18:59:58 +0530

    Add axios API client and AuthContext for global auth state

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001/api', 
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (!window.location.pathname.includes('/auth')) {
+        window.location.href = '/auth/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
